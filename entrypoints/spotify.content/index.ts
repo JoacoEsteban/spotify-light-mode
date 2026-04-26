bde65414cb560f759bfc9ce4104a525876349160
Joaquin Esteban <dev@joaco.io>
2026-04-26 17:32:44 -0300

    convert spotify-light CSS index to TypeScript module fixing hot reload
    - Replace auto-generated index.css with index.ts
    - Update generate-spotify-light-css.ts to output TS instead of CSS
    - Update content script import accordingly

--- entrypoints/spotify.content/index.ts
+++ entrypoints/spotify.content/index.ts
@@ -5,7 +5,7 @@ import {
   readEnabled,
   readUseSystemPref,
 } from "../../lib/storage";
-import lightModeCss from "../../assets/spotify-light/index.css?inline";
+import lightModeCss from "../../assets/spotify-light/index";
 import { createInlineStyleObserver } from "./inline-style-observer";
 
 export default defineContentScript({
